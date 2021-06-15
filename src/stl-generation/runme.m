ae41bad2b436c919948aedc61eedc4a118deb493
Loris Fichera <loris.fichera@gmail.com>
2021-06-15 15:39:44 -0400

    The code is now able to generate filenames independently from the specific dataset

--- src/stl-generation/runme.m
+++ src/stl-generation/runme.m
@@ -6,19 +6,16 @@
 %                 Alex Chiluisa <ajchiluisa@wpi.edu>
 %                 Loris Fichera <lfichera@wpi.edu>
 %
-% Latest version: 6/14/2021
+% Latest version: 6/15/2021
 
 % Clean the workspace and close all the figures
 clear, close all, clc
 
 % What model are we working with?
-modelID = 'L7'; % model ID
-p = '\\research.wpi.edu\ROBOTICS\comet\NIDCD-2020\Vocal-Folds-Models-Bailly\LARYNX7\LARYNX7\L7a_pag_concaténé'; % path where the images are stored
-prefix = 'L7a_pag_concat'; % image prefix
+modelID = 'L8C1'; % model ID
+p = '\\research.wpi.edu\ROBOTICS\comet\NIDCD-2020\Vocal-Folds-Models-Bailly\LARYNX8\LARYNX8\L8a_pag_concaténé'; % path where the images are stored
+prefix = 'L8a_pag_concat'; % image prefix
 binarization_threshold = 29000;
-
-% Define the path where the images are stored
-addpath(p);
 
 % Read the image size
 i = imread(fullfile(p, [prefix '0000.tif']));
@@ -33,7 +30,7 @@ xLB = roi.XLowerBound; xUB = roi.XUpperBound;
 yLB = roi.YLowerBound; yUB = roi.YUpperBound;
 
 % Define the sampling interval across the three axes
-dZ = 2; dY = 2; dX = 2;
+dZ = 2; dY = 5; dX = 5;
 
 % Initialize the matrix where we are going to store all the images
 VocalFolds = zeros((yUB - yLB)/dY, ...
@@ -49,26 +46,28 @@ for k = 0:dZ:(Z-1)
     idx = 1;
     idy = 1;
     
-  % !FIXME the following code is not portable
+  % Generate the image file name
   if k>9 && k<100
-      myfilename = sprintf('L7a_pag_concat00%d.tif', k);
+      filename = [prefix '00' num2str(k) '.tif'];
   elseif k>99 && k<1000
-       myfilename = sprintf('L7a_pag_concat0%d.tif', k);
+      filename = [prefix '0' num2str(k) '.tif'];
   elseif k>999 && k<10000
-       myfilename = sprintf('L7a_pag_concat%d.tif', k);
+      filename = [prefix num2str(k) '.tif'];
   else
-      myfilename = sprintf('L7a_pag_concat000%d.tif', k);
+      filename = [prefix '000' num2str(k) '.tif'];
   end
 
-  raw_image = imread(myfilename);             % Read the image
+  raw_image = imread(fullfile(p, filename));  % Read the image
   cropped_image = raw_image(yLB:yUB,xLB:xUB); % Crop the image
   downscaled_image = zeros((yUB-yLB)/dY,(xUB-xLB)/dX); % Initialize the downscaled image
  
-  for p = 1 : dX : (xUB-xLB)
+  % Downsample the image
+  for r = 1 : dX : (xUB-xLB)
       for q = 1 : dY : (yUB-yLB)
-          downscaled_image(idx,idy) = cropped_image(q,p);
+          downscaled_image(idx,idy) = cropped_image(q,r);
           idx=idx+1; 
       end
+      
       idx = 1;
       idy = idy+1;
   end
@@ -78,14 +77,15 @@ for k = 0:dZ:(Z-1)
   BW = zeros(L,W);  
   BW = downscaled_image > binarization_threshold;
               
-  VocalFolds(:,:,idz) = BW;                                       % match dimensions
+  VocalFolds(:,:,idz) = BW; % Save the result in the VocalFolds variable
   idz = idz + 1;
+  
   waitbar(k/(Z-1),f,'Processing Images...');
 end
 
 close(f)
 
-make_STL_of_Array('L7aV3.stl',VocalFolds,...
-                  VoxelSizeX * dX,...
-                  VoxelSizeY * dY,...
-                  VoxelSizeZ * dZ);
+make_STL_of_Array([modelID '.stl'],VocalFolds,...
+                  roi.VoxelSizeX * dX,...
+                  roi.VoxelSizeY * dY,...
+                  roi.VoxelSizeZ * dZ);
