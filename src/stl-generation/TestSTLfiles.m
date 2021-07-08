8fbbc4e015f8c9abdbb1df7ed7e35ac571a69517
alexchiluisac <44651550+alexchiluisac@users.noreply.github.com>
2021-07-08 15:19:30 -0400

    stl generation

--- src/stl-generation/TestSTLfiles.m
+++ src/stl-generation/TestSTLfiles.m
@@ -4,11 +4,14 @@
 % Select the larynx model you want to visualize acording with the folder
 % name.
 
-%clear all; close all; clc;
+clear all; close all; clc;
 
-modelID = 'larynx7a';
+modelID = 'larynx8b';
 
-%%
+%
+p = [ 0.8       0.8       1.0;    % green
+      0.2422    0.1504    0.6603; % purple
+      0.9769    0.9839    0.0805]; % yellow
 % Files that we are going to read
 file1 = 'tissue.stl';
 file2 = 'tissue_cropped.stl';
@@ -19,14 +22,15 @@ Stl2 = fullfile('..', '..', 'anatomical-models', modelID, file2);
 [V1,F1,Name1] = stlRead(Stl1);
 [V2,F2,Name2] = stlRead(Stl2);
 
+
 %% Plot the STL files 
 
 % Plot the larynx for calcReachableSpace function 
 figure
-stlPlot(V1,F1,Name1);
+stlPlot(V1,F1,Name1,p(2,:));
 title(upper(file1(1:6)));
 
 % Plot the larynx model for calcVisibleArea function
 figure
-stlPlot(V2,F2,Name2);
+stlPlot(V2,F2,Name2,p(2,:));
 title('TISSUE CROPPED');
