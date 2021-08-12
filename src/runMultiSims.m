1d5f7547f3fa2cfa24f25a03a32670a15211eb88
alexchiluisac <44651550+alexchiluisac@users.noreply.github.com>
2021-08-12 11:12:33 -0400

    Update runMultiSims.m

--- src/runMultiSims.m
+++ src/runMultiSims.m
@@ -15,20 +15,33 @@ addpath('kinematics', 'utils', 'figure-generation', 'path-planning', ...
 
 %% Constants for the simulations
 % Anatomical model definition
-modelID = 'larynx1';  % ID of the anatomical model (see the `anatomical-models' folder)
+modelID = 'larynx7b';  % ID of the anatomical model (see the `anatomical-models' folder)
 
 % Simulation parameters
 nPoints = 100; % number of configurations sampled by RRT
-dq = 0.06;    % step size for rrt
-laserOffsetAngle = 90;
-useWrist = true;
+dq = 0.03;    % step size for rrt
+laserOffsetAngle = 0;
+useWrist = false;
 
 % Endoscope geometry definition
 %  The variable naming used in this section is consistent with (Chiluisa et al. ISMR 2020)
 n = 10; % number of cutouts
 viewang = deg2rad(85);
-u = [1.8646e-3 * ones(1,n-1), 1.86e-3]; % notch spacing [m]
-h = 0.1191e-3 * ones(1,n);         % notch height  [m]
+% u = [1.8646e-3 * ones(1,n-1), 1.86e-3]; % notch spacing [m]
+% h = 0.1191e-3 * ones(1,n);         % notch height  [m]
+
+%ADDED THESE LINES TO HAVE THE SAME CODE THAN RUNSIMULATION
+%---------
+R = 8;
+%L = calc_L(viewang, R, 2.5, 0, 0);
+L = 20;
+[singleH, singleU] = calc_config(L, R, n, 1.1/2, 0.9/2, 0.935);
+
+%u = 0.000367766480556499;
+%h = 0.000187789074999056;
+u = singleU*1e-3 * ones(1,n); % notch spacing [m]
+h = singleH*1e-3 * ones(1,n);         % notch height  [m]
+%---------------
 
 if useWrist
     % wrist configuration
@@ -38,9 +51,9 @@ if useWrist
 else
     fprintf("Wrist is turned off!!\n")
     % laser configuration
-    w = 0.50e-3 * ones(1,n);          % notch width   [m]
+    w = 0.40e-3 * ones(1,n);          % notch width   [m]
     OD = 0.60e-3;                     % endoscope outer diameter [m]
-    ID = 0.20e-3;                     % endoscope inner diameter [m]
+    ID = 0.40e-3;                     % endoscope inner diameter [m]
 end
 
 if laserOffsetAngle
@@ -49,10 +62,10 @@ end
 
 %% Parameters to vary
 
-params = [100];
+params = [10000 15000 20000];
 
 %% Set up csv to collect data
-filename = 'Simulation-Larynx1-numberNotches.csv';
+filename = ['Simulation-', modelID, '-numberNotches.csv'];
 
 otherinfo = [];
 if ~useWrist
@@ -69,7 +82,8 @@ simIDs = {};
 parfor pp = 1 : len
     nPoints = params(pp);
     
-    simulationID = [modelID otherinfo '-nNotches-' num2str(n) '-' num2str(nPoints) 'pts'];    % specific workspace id
+    %simulationID = [modelID otherinfo '-nNotches-' num2str(n) '-' num2str(nPoints) 'pts'];    % specific workspace id
+    simulationID = [modelID otherinfo '-dq-' num2str(dq) '-' num2str(nPoints) 'pts'];    % specific workspace id
     simIDs{pp} = simulationID;
     
     fprintf('** Beginning simulation %d of %d ID: %s ** \n', pp, len, simulationID)
@@ -77,7 +91,7 @@ parfor pp = 1 : len
     calcVisibleArea(simulationID, 'mcrc', laserOffsetAngle);                          % runs raycasting
     % record important data
     getSimData(simulationID, filename);
-    %animateResults(simulationID);
+    animateResults(simulationID);
 end
 
 %% display figures
@@ -85,7 +99,7 @@ simIDs = convertCharsToStrings(simIDs);
 for i=1:length(simIDs)
     simulationID = simIDs{i};
     makeVisibilityFig(simulationID); 
-    %savefig(['figures/' simulationID '.fig']);
+    savefig(['figures/' simulationID '.fig']);
 end
 
 fintime = toc(start);
