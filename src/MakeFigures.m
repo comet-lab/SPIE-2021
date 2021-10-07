fec3a472e4cd9981e0ed5d9616382240ea745f81
evminch <84356737+evminch@users.noreply.github.com>
2021-10-07 12:39:08 -0400

    Figures Updated Parameters

--- src/MakeFigures.m
+++ src/MakeFigures.m
@@ -32,6 +32,11 @@ end
 figure('Name', simulationID);
 stlPlot(meMesh.vertices * 1e3, meMesh.faces, 'Visibility', v);
 axis equal
-view(-36.9518,  29.7949)
-set(gca, 'fontsize', 14);
+view(-45, 35)
+% larynx 1 az = -45, el = 35
+% larynx 2 az = -135, el = 35
+zlim([-59 -18])
+% larynx 1 zlim zmin = -59, zmax = -18
+% larynx 2 zlim zmin = -60, zmax = -15
+set(gca, 'fontsize', 18);
 title('Steerable Fiber');
