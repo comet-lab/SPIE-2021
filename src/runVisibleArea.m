15125a6a41a77a0814ac265b4ec6ae22474129a9
Alex Chiluisa C <44651550+alexchiluisac@users.noreply.github.com>
2021-10-19 11:14:35 -0400

    Update runVisibleArea.m

--- src/runVisibleArea.m
+++ src/runVisibleArea.m
@@ -1,19 +1,19 @@
 clc; close all;
 
-% %% Load the mat file that you want to run
-% 
-% matfile = 'larynx8a-nowrist-dq-0.06-10000pts.mat';
-% load(matfile);
+%% Load the mat file that you want to run
+
+matfile = 'larynx7b-nowrist-dq-0.06-10000pts.mat';
+load(matfile);
 
 %%
 % First, load the mat file
 
 %% offste angle [0 45 70 90] deg
 useWrist = false;
-laserOffsetAngle = 90;
+laserOffsetAngle = 45;
 
 %% Anatomical model definition
-modelID = 'larynx7a'; % ID of the anatomical model (see the `anatomical-models' folder)
+modelID = 'larynx7b'; % ID of the anatomical model (see the `anatomical-models' folder)
 
 otherinfo = [];
 
@@ -40,11 +40,10 @@ telapsed = toc(tstart);
 s = seconds(telapsed);
 s.Format = 'hh:mm:ss'
 %% Create a video of this simulation and Histogram
-animateResults(simulationID);
 makeVisibilityFig(simulationID);
-
 filename = 'testSim.csv';
 getSimData(simulationID, filename);
-
 % Save figure to folder
 savefig(['figures/' simulationID '.fig']);
+
+animateResults(simulationID);
