e2fb602fdb9a622d7de1812c0a12cb5293756596
Loris Fichera <loris.fichera@gmail.com>
2022-06-07 16:03:04 -0400

    Update makeVisibilityFig2.m

--- src/figure-generation/makeVisibilityFig2.m
+++ src/figure-generation/makeVisibilityFig2.m
@@ -1,4 +1,4 @@
-function makeVisibilityFig(simulationID, meMesh, visibleMapTotal, visibleMapTotalCamera)
+function makeVisibilityFig2(simulationID, meMesh, visibleMapTotal, visibleMapTotalCamera)
 % Produces figures were the visible area of a model is highlighted
 %
 % simulationID = identifier of this simulation
@@ -8,20 +8,28 @@ function makeVisibilityFig(simulationID, meMesh, visibleMapTotal, visibleMapTota
 
 % load([simulationID '.mat']);
 
-figure('Name', simulationID);
+f = figure('Name', simulationID);
+f.Position = [1474, 152, 1090, 643];
+
+c = [[133 122 189] ./ 255;
+     1 0 0];
+
 subplot(131);
 v = logical(visibleMapTotal);
 stlPlot(meMesh.vertices * 1e3, meMesh.faces, 'Visibility', v);
-title('Laser Reachability');
+title('(a) Laser Fiber Reachability');
+set(gca,'FontSize',16);
 
 subplot(132);
 v = logical(visibleMapTotalCamera);
 stlPlot(meMesh.vertices * 1e3, meMesh.faces, 'Visibility', v);
-title('Reachability & Visibility');
+title({'(b) Laser Fiber Reachability +','Endoscope Visibility'});
+set(gca,'FontSize',16);
 
 subplot(133);
 v = logical(visibleMapTotal) - logical(visibleMapTotalCamera);
-stlPlot(meMesh.vertices * 1e3, meMesh.faces, 'Visibility', v);
-title('Difference');
+stlPlot(meMesh.vertices * 1e3, meMesh.faces, 'Visibility', v, c);
+title('Difference between (a) and (b)');
+set(gca,'FontSize',16);
 
 end
